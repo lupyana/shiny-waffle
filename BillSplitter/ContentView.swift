6b1235f810f34fd469e7ae2677227d7a1944b006
Lupyana <lupyanambembati@gmail.com>
2019-11-06 17:53:32 +0300

    Feat: Some ui changes
    - Added Textfield for number of people
    - Added Textfield for bill value
    - Added a slider to selelect tip of any
    - Added button to calculate the total bill

--- BillSplitter/ContentView.swift
+++ BillSplitter/ContentView.swift
@@ -9,8 +9,68 @@
 import SwiftUI
 
 struct ContentView: View {
+       @State var totalClicked: Int = 0
+        @State var TotalBill = ""
+        @State var numOfPeople = ""
+       @State var sliderValue = 0.0
+    @State var payable_bill: Double = 0.0
+       var minimumValue = 0.0
+       var maximumvalue = 20.0
+    
     var body: some View {
-        Text("Hello World")
+        VStack() {
+            Text("Bill Splitter")
+            .bold()
+            .font(.title)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
+            
+            HStack() {
+                Text("What is the total bill ?")
+                Spacer()
+            }
+            
+            TextField("Total Bill Value", text: $TotalBill)
+                .padding()
+                .background(Color.white)
+                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
+            
+            HStack() {
+                Text("How many people do you want to split with ?")
+                Spacer()
+            }
+              
+            
+            TextField("Total number of people", text: $numOfPeople)
+                .padding()
+                .background(Color.white)
+                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+            
+            HStack() {
+                Text("Tips ?")
+                Spacer()
+            }
+            
+            Slider(value: $sliderValue, in: minimumValue...maximumvalue)
+                .padding()
+            
+            Text("\(Int(sliderValue)) $")
+          
+            
+            Spacer()
+            
+            Button(action: {
+                self.payable_bill =  3.0 / 3.0
+                print(self.payable_bill)
+                self.totalClicked = self.totalClicked + 1}) {
+                Spacer()
+                Text("Split Bill")
+                    .foregroundColor(Color.white)
+                    .bold()
+                Spacer()
+            }.padding().background(Color.green).cornerRadius(4.0)
+            
+        }.padding().background(Color.blue.edgesIgnoringSafeArea(.all))
     }
 }
 
